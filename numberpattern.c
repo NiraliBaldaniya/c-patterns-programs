4e07b59cdc9c9fb8d694e5732b959d2f7bb2ecf4
NiraliBaldaniya <niralibaldaniya17@gmail.com>
2026-01-24 18:24:37 +0530

    add number pattern

---
+++ numberpattern.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number of rows: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+}
+
+//output:
+//Enter a number of rows: 4
+//1
+//22
+//333
+//4444
